cb92a00f95e4c487799f06e6b075a48ac8cd2aed
faris096 <farish.sirraj@gmail.com>
2022-05-15 16:07:24 +0800

    15th May

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors, avoid_unnecessary_containers
+import 'package:flutter/material.dart';
+import 'settings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage(), routes: <String, WidgetBuilder>{
+      '/book': (context) => Book(),
+      '/settings': (context) => Settings()
+    });
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(0, 74, 173, 2),
+        body: Center(
+            child: Column(children: <Widget>[
+          Container(child: Image(image: AssetImage('assets/logo.png'))),
+          SizedBox(
+            width: 280.0,
+            height: 50.0,
+            child: ElevatedButton(
+                child: Text(
+                  'bB O O K',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/book');
+                }),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 280.0,
+            height: 50.0,
+            child: ElevatedButton(
+                child: Text(
+                  'S E T T I N G S',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/settings');
+                }),
+          )
+        ])));
+  }
+}
+
+class Book extends StatelessWidget {
+  //const Book({Key? key}) : super(key: key);
+
+  List<String> items = <String>['1', '2'];
+  String? electedItem = '1';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('B O O K'),
+        backgroundColor: const Color.fromRGBO(0, 74, 173, 2),
+      ),
+    );
+  }
+}
+
+
+
+/*
+child: Text(
+                  'S A V E  |  U P D A T E',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                ),
+                onPressed: null,
+                */
---
+++ lib/settings.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('S E T T I N G S'),
+        backgroundColor: const Color.fromRGBO(0, 74, 173, 2),
+      ),
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "NUS Email ID",
+                  labelText: "Email",
+                ),
+              )),
+          Padding(padding: EdgeInsets.all(0.5)),
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Phone Number",
+                  labelText: "Phone Number",
+                ),
+              )),
+          Padding(padding: EdgeInsets.all(10)),
+          SizedBox(
+            width: 350.0,
+            child: FloatingActionButton.extended(
+              onPressed: () {},
+              label: Text('S A V E'),
+              icon: Icon(Icons.save),
+              backgroundColor: Color.fromRGBO(0, 74, 173, 2),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
