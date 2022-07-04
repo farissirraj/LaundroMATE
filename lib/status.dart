05cc620a6fc8c8922951088bab62bd944a45d65d
faris096 <farish.sirraj@gmail.com>
2022-07-05 00:41:48 +0800

    Cleaned Updated

--- lib/status.dart
+++ lib/status.dart
@@ -1,22 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-// import 'book.dart';
-// import 'dart:math';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/scheduler.dart';
-// import 'package:syncfusion_flutter_calendar/calendar.dart';
-// import 'package:intl/intl.dart';
-// import 'settings.dart';
-// import 'main.dart';
-// import 'package:url_launcher/url_launcher.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'globals.dart' as globals;
-
-//int status = 0;
 
 checkIfDocExists() async {
   DocumentSnapshot ds = FirebaseFirestore.instance
@@ -39,7 +23,7 @@ class StatusOne extends StatelessWidget {
         backgroundColor: const Color.fromRGBO(255, 255, 255, 2),
         body: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               width: double.infinity,
               height: 50,
             ),
@@ -47,7 +31,7 @@ class StatusOne extends StatelessWidget {
                 width: double.infinity,
                 height: 200,
                 child: Image(image: AssetImage(globals.wmPath))),
-            SizedBox(
+            const SizedBox(
               width: double.infinity,
               height: 50,
             ),
